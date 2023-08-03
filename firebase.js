f3156c09d8020b5b801678476deb9cf6cb50427a
Yvan <105001832+YVANDD@users.noreply.github.com>
2023-08-03 11:00:07 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxe2w1LlwSh-21vHRmdhHtXRbs0rKLwJM",
+  authDomain: "clone-two-52ff1.firebaseapp.com",
+  projectId: "clone-two-52ff1",
+  storageBucket: "clone-two-52ff1.appspot.com",
+  messagingSenderId: "761626137257",
+  appId: "1:761626137257:web:2ecaa0e4154b3166570fb8",
+  measurementId: "G-DQ55PS5M45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
